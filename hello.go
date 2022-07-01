bf3d8c30259cc6552445f1596d9ee213147127f7
pedro.dantas <pedro.dantas@mobicare.com.br>
2022-07-01 07:55:35 -0300

    switch case

--- hello.go
+++ hello.go
@@ -3,5 +3,20 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World")
+	fmt.Println("1 - iniciar monitoramento")
+	fmt.Println("2 - Exibir logs")
+	fmt.Println("3 - Sair do programa")
+	var command int
+	fmt.Scan(&command)
+
+	switch command {
+	case 1:
+		fmt.Println("Case 01")
+	case 2:
+		fmt.Println("Case 2")
+	case 3:
+		fmt.Println("Case 3")
+	default:
+		fmt.Println("Default")
+	}
 }
