5736fcc8e290d0f14d31f60e0a41274469bb0a89
pedroppd <pedro_pdantas@hotmail.com>
2022-07-03 17:10:10 -0300

    Reading a file in GO

--- src/hello.go
+++ src/hello.go
@@ -3,18 +3,20 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 	"time"
 )
 
 const DELAY = 30 * time.Second
-
+const FILE_PATH = '';
 func main() {
 	showOptions()
 	callMethod()
 }
 
 func startMonitoring() {
-	sites := []string{"https://www.google.com", "https://www.alura.com.br", "https://www.facebook.com", "https://www.twitter.com"}
+	//sites := []string{"https://www.google.com", "https://www.alura.com.br", "https://www.facebook.com", "https://www.twitter.com"}
+	sites := readFile(FILE_PATH);
 	for {
 		for _, site := range sites {
 			fmt.Println("Starting monitoring: ", site)
@@ -54,4 +56,11 @@ func showOptions() {
 	fmt.Println("1 - iniciar monitoramento")
 	fmt.Println("2 - Exibir logs")
 	fmt.Println("3 - Sair do programa")
+}
+
+func readFile(filePath string) {
+	stream, err := os.Open(filePath)
+	if err == nil {
+		fmt.Println(stream)
+	}
 }
